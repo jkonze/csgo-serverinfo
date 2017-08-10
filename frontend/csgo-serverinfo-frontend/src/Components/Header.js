91e464ac270ffb2835ce718c0933a959bf4bd937
braams <braams@informationsgesellschaft.com>
2017-08-10 13:49:46 +0200

    - small layout changes

--- frontend/csgo-serverinfo-frontend/src/Components/Header.js
+++ frontend/csgo-serverinfo-frontend/src/Components/Header.js
@@ -7,10 +7,8 @@ export default class Header extends Component{
     render(){
         return(
             <header className="header">
-
                 <h1 className="header__title">Choose one of our servers:</h1>
-                <h3 className="header__subtitle">Kindly provided for your enjoyment from <a href="http://steamcommunity.com/id/wasnlosdu" target="_blank">wasnlosdu</a> &amp; <a href="http://steamcommunity.com/id/-max" target="_blank">_fat</a></h3>
-
+                <h3 className="header__subtitle">Kindly provided for your enjoyment from <a href="http://steamcommunity.com/id/wasnlosdu">wasnlosdu</a> &amp; <a href="http://steamcommunity.com/id/-max">_fat</a></h3>
             </header>
         )
     }
